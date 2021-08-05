550a31a4097195a241611e3319ea7ef9868ed17d
jrgdiaz <jrgdiaz809@gmail.com>
2021-08-05 00:13:14 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,18 @@
+#proof of concept to find potentially "UI-Redressable" subdomains using OSINT module
+
 from redteam import RedTeam
+from datetime import datetime
 
-x = RedTeam('github.com')
+now = datetime.now() # get current date and time
+date_time = now.strftime("%m%d%Y%H%M%S")
+domain = 'example.org'
+x = RedTeam(domain)
 subdomains = x.subdomain_recon()
-
-print("=====================reconocimiento de activos en base a certificados digitales publicados en internet========================")
-
+# store results in a file
+file_ = open("resources/clickjacking_%s_%s.txt" % (domain, date_time),'x')
 for subdomain in subdomains:
-    print(subdomain)
-
-print("=====================dns dumpster dive=====================")
-
-x.dnsdumpster()
-
-print("=====================dns axfr test=====================")
-
-x.dnsaxfr()
+    missing_headers = x.xwebheaders(subdomain)
+    if 'X-Frame-Options' in missing_headers:
+        print(subdomain+" is vulnerable to clickjacking!")
+        file_.write(subdomain+" is vulnerable to clickjacking!\n")   
+file_.close()
