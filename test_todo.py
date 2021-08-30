7ae362ddbf323c507881b6e1d2a4e752de7b2fcc
jrgdiaz <jrgdiaz809@gmail.com>
2021-08-30 15:59:36 -0400

    Update test_todo.py

--- test_todo.py
+++ test_todo.py
@@ -1,6 +1,6 @@
 from redteam import RedTeam
-import workspace
 from datetime import datetime
+
 wordlist_file = "quickhits.txt"
 now = datetime.now() # get current date and time
 date_time = now.strftime("%m%d%Y%H%M%S")
@@ -8,5 +8,4 @@ domain = 'example.org'
 x = RedTeam(domain)
 subdomains = x.subdomain_recon()
 tasks = [lambda subdomain=subdomain: x.dir_bruter("http://"+subdomain,wordlist_file,wildcard=False) for subdomain in subdomains]
-workspace.run_io_tasks_in_parallel(tasks,len(tasks))
-
+x.run_io_tasks_in_parallel(tasks,len(tasks))
