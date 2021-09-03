676e998a11b165584be1d9831802cbf57fd9308c
jrgdiaz <jrgdiaz809@gmail.com>
2021-09-02 23:44:57 -0400

    Create test_todo.py

---
+++ examples/test_todo.py
@@ -0,0 +1,11 @@
+from redteam import RedTeam
+from datetime import datetime
+
+wordlist_file = "quickhits.txt"
+now = datetime.now() # get current date and time
+date_time = now.strftime("%m%d%Y%H%M%S")
+domain = 'example.org'
+x = RedTeam(domain)
+subdomains = x.subdomain_recon()
+tasks = [lambda subdomain=subdomain: x.dir_bruter("http://"+subdomain,wordlist_file,wildcard=False) for subdomain in subdomains]
+x.run_io_tasks_in_parallel(tasks,len(tasks))
