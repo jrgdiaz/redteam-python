f91159b4e39103ed9270f6cd1421172d45dec94b
jrgdiaz <jrgdiaz809@gmail.com>
2021-10-30 11:24:56 -0400

    Delete googledork_hackerone_reports.py

--- examples/googledork_hackerone_reports.py
+++
@@ -1,7 +0,0 @@
-from redteam import RedTeam
-x = RedTeam()
-query = "Manage Engine"
-hits = x.googledork("site: hackerone.com -site:docs.hackerone.com inurl:reports "+query,60)
-for hit in hits:
-        response = requests.get(hit)
-        print(hit)
